12309487edf9edc1c7c767e2530c9800fb965289
Allwell Agwu-Okoro <allwell@allwell.dev>
2024-04-23 02:48:49 +0100

    update ssh_keygen.sh

--- scripts/ssh_keygen.sh
+++ scripts/ssh_keygen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Check for existing SSH keys and generate a new one if none exists
-if [ ! -f "/home/vagrant/.ssh/id_rsa" ]; then
+if [ ! -f "/home/vagrant/.ssh/id_rsa_slavevm" ]; then
   echo "Generating SSH key..."
-  sudo -u vagrant ssh-keygen -t rsa -b 4096 -C "slave vm pka created by Allwell 220424" -N "" -f "/home/vagrant/.ssh/id_rsa"
+  sudo -u vagrant ssh-keygen -t rsa -b 4096 -C "slave vm pka created by Allwell 220424" -N "" -f "/home/vagrant/.ssh/id_rsa_slavevm"
   echo "SSH key generated."
-  sudo -u vagrant chmod 400 /home/vagrant/.ssh/id_rsa
+  sudo -u vagrant chmod 400 /home/vagrant/.ssh/id_rsa_slavevm
 else
   echo "SSH key already exists."
 fi
